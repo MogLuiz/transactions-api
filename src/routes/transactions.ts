86d2c1f285022b92954dfaec9ba6c5d1566691d8
Luiz Henrique <luizhjramos@outlook.com>
2023-02-28 22:36:46 -0300

    feat: list transactions route

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -6,12 +6,18 @@ import { createTransactionBodySchema } from '../schemas'
 import { knex } from '../database'
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select()
+
+    return { transactions }
+  })
+
   app.post('/', async (request, reply) => {
     const { title, amount, type } = createTransactionBodySchema.parse(
       request.body,
     )
 
-    await knex('234').insert({
+    await knex('transactions').insert({
       id: randomUUID(),
       title,
       amount: type === 'credit' ? amount : amount * -1,
