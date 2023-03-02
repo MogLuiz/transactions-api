d5b0d49b2508e8d8768aa7f6639bb6bb31af7319
Luiz Henrique <luizhjramos@outlook.com>
2023-03-02 07:07:59 -0300

    test: ensure be able to list all transactions

--- src/test/transactions.test.ts
+++ src/test/transactions.test.ts
@@ -1,4 +1,4 @@
-import { afterAll, beforeAll, test, expect } from 'vitest'
+import { afterAll, beforeAll, it, expect, describe } from 'vitest'
 import request from 'supertest'
 import { app } from '../app'
 
@@ -10,12 +10,38 @@ afterAll(async () => {
   await app.close()
 })
 
-test('should user create a new transaction', async () => {
-  const response = await request(app.server).post('/transactions').send({
+describe('Transactions routes', () => {
+  const transactionData = {
     title: 'New transaction',
     amount: 5000,
     type: 'credit',
+  }
+
+  it('should user create a new transaction', async () => {
+    const response = await request(app.server)
+      .post('/transactions')
+      .send(transactionData)
+
+    expect(response.statusCode).toBe(201)
   })
 
-  expect(response.statusCode).toBe(201)
+  it('should be able to list all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send(transactionData)
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: transactionData.title,
+        amount: transactionData.amount,
+      }),
+    ])
+  })
 })
